140de6e8b3795eaf71688aaff41c9061407b7730
Ayushkr7380 <ayushkumar19999@gmail.com>
2024-09-04 20:36:44 +0530

    number variable

--- day1/variableType.js
+++ day1/variableType.js
@@ -1,2 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 var name1 = 'ayush';
 console.log(name1);
--- day1/variableType.ts
+++ day1/variableType.ts
@@ -1,3 +1,14 @@
+//string 
 let name1:string = 'ayush';
 console.log(name1);
+
+//number 
+//In TypeScript we have number varaible , only number varaible , there is no float or int....
+
+let roll:number = 123;
+console.log(roll);
+
+let price:number = 200.45;
+console.log(price);
+
 export {}
