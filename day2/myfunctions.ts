52c81ef01670dbc2bfc69ee3e0f614cbea1ae6b7
Ayushkr7380 <ayushkumar19999@gmail.com>
2024-09-05 11:20:47 +0530

    functions

--- day2/myfunctions.ts
+++ day2/myfunctions.ts
@@ -1,12 +1,16 @@
 function addnum(num1:number){
     return num1
 }
-
 addnum(5);
 
 function sum1(n1:number){
     return n1 + 10;
 }
 sum1(23);
+
+function number1(s1:number){
+    return s1.toFixed;
+}
+number1(12.5657);
 
 export {}
