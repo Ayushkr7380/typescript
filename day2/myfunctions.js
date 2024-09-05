2de6764efa46b56c1d6bf360d6f363c3745d6f2a
Ayushkr7380 <ayushkumar19999@gmail.com>
2024-09-05 11:17:46 +0530

    functions

---
+++ day2/myfunctions.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function addnum(num1) {
+    return num1;
+}
+addnum(5);
---
+++ day2/myfunctions.ts
@@ -0,0 +1,12 @@
+function addnum(num1:number){
+    return num1
+}
+
+addnum(5);
+
+function sum1(n1:number){
+    return n1 + 10;
+}
+sum1(23);
+
+export {}
