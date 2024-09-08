b49aa5d284a3511b81538f2588cebeb24ab8d443
Ayushkr7380 <ayushkumar19999@gmail.com>
2024-09-08 21:42:57 +0530

    more in interface

--- day3/myInterface.js
+++ day3/myInterface.js
@@ -17,3 +17,21 @@ var data = {
     },
     isAdmin: false
 };
+var ayushAdmin = {
+    isPrivate: true,
+    _id: "123432",
+    name: 'ayush',
+    email: "a@a.com",
+    roll: '123',
+    isActive: true,
+    startTrail: function () {
+        return "i am string";
+    },
+    startTrail1: function () {
+        return 123;
+    },
+    getCoupon: function (name) {
+        return 10;
+    },
+    isAdmin: false
+};
--- day3/myInterface.ts
+++ day3/myInterface.ts
@@ -34,6 +34,32 @@ let data : user = {
     isAdmin : false
 }
 
+//Also we can extend all the type from one interface to another and add more into that  ....
+
+interface Admin extends user{
+    isPrivate :boolean
+}
+
+const ayushAdmin : Admin ={
+    isPrivate : true,
+    _id : "123432",
+    name : 'ayush',
+    email : "a@a.com",
+    roll : '123',
+    isActive : true,
+    startTrail : ()=>{
+        return "i am string"
+    },
+    startTrail1:()=> {
+        return 123
+    },
+    getCoupon :(name : "ayush10")=>{
+        return 10
+    },
+    isAdmin : false
+
+}
+
 
 
 export {} 
