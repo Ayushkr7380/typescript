53995fc2b075dacbbc96d77cda709df505ca13ef
Ayushkr7380 <ayushkumar19999@gmail.com>
2024-09-10 22:34:19 +0530

    myTuples more

--- day4/myTuples.ts
+++ day4/myTuples.ts
@@ -1,4 +1,7 @@
 let user : [number,string];
 user = [12,'ayush'];
-
+//if we have set only 2 datatype in user then we can not add any more datatype..
 user[0] = 54;
+
+type stuData = [number,string,string];
+let details : stuData = [1,'ayush','a']
