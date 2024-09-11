db221f6f6d29097f94ddfa5d19345c1f793e8001
Ayushkr7380 <ayushkumar19999@gmail.com>
2024-09-11 18:49:06 +0530

    private

--- day5/myPrivatePublic.ts
+++ day5/myPrivatePublic.ts
@@ -1,3 +1,4 @@
+//basic class and object 
 class User {
     email : string
     name : string
@@ -9,5 +10,22 @@ class User {
 }
 
 const obj = new User('a@a.com','ayush');
-console.log(obj)
+console.log(obj);
+
+//private and public in typescript classes
+class User1{
+    email : string
+    name : string
+    private id : string
+    constructor(email : string , name : string , id : string ){
+        this.email = email
+        this.name = name
+        this.id = id
+    }
+}
+
+const obj1 = new User1('ayush@123.com','ayush','123');
+console.log(obj1.email); //Here we can easily log email as it is not set private..
+// console.log(obj1.id); // Here we can not log the id as we have set it as private..so it is not accessible inside the class itself.. 
+
 export {}
