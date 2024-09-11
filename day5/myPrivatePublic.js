b06dbdb8782532f4468e87755a730192b0827fd6
Ayushkr7380 <ayushkumar19999@gmail.com>
2024-09-11 18:49:31 +0530

    private

--- day5/myPrivatePublic.js
+++ day5/myPrivatePublic.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+//basic class and object 
 var User = /** @class */ (function () {
     function User(email, name) {
         this.city = 'muz';
@@ -10,3 +11,14 @@ var User = /** @class */ (function () {
 }());
 var obj = new User('a@a.com', 'ayush');
 console.log(obj);
+//private and public in typescript classes
+var User1 = /** @class */ (function () {
+    function User1(email, name, id) {
+        this.email = email;
+        this.name = name;
+        this.id = id;
+    }
+    return User1;
+}());
+var obj1 = new User1('ayush@123.com', 'ayush', '123');
+console.log(obj1.email); //Here we can easily log email as it is not set private..
