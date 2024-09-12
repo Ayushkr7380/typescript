aec82b4afdf486225f350698f6a77c4df8f6d086
Ayushkr7380 <ayushkumar19999@gmail.com>
2024-09-12 17:37:25 +0530

    myGeneric1

---
+++ day6/myGeneric1.js
@@ -0,0 +1,6 @@
+//Normal way to create array ..
+var heros = ["ayush", "ayush2", "ayush3"];
+var data = ["ayush", 123, true, 3454];
+//Generic way to create array ..
+var heros2 = ["ayush", "ayush2", "ayush3"];
+var data1 = [true, false, 1234, "ayush"];
---
+++ day6/myGeneric1.ts
@@ -0,0 +1,7 @@
+//Normal way to create array ..
+let heros : string[] = ["ayush","ayush2","ayush3"];
+let data : (string | number | boolean )[] =  ["ayush",123,true , 3454];
+
+//Generic way to create array ..
+let heros2 : Array<string> = ["ayush","ayush2","ayush3"];
+let data1 : Array<string | number | boolean> = [true,false,1234,"ayush"];
